aeb46f38fc68915eef261d7748b3c9947fc3b89c
lilongWork <lilongcnc@vip.qq.com>
2016-08-17 17:32:06 +0800

    提交圆圈遮罩转场动画

---
+++ LLPinTransitionDemo/LLPinTransitionDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LLPinTransitionDemo
+//
+//  Created by 李龙 on 16/8/17.
+//  Copyright © 2016年 李龙. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()<UINavigationControllerDelegate,UIViewControllerTransitioningDelegate>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
